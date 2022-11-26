ac4958290b0cf9259df034d9cd5b546f4af28bb7
Vladimir <bigcorvet@gmail.com>
2022-11-26 11:34:19 +0200

    add def choice,  start

--- black_jack.py
+++ black_jack.py
@@ -27,6 +27,42 @@ class BlackJack:
         self.print_cart(current, score, bot)
         return score
     
-    
+    def choice(self):
+        score = self.random_cart(self.score, False)
+        bot_score = self.random_cart(self.score, True)
+        while True:
+            choice = input('Будете брать карту? y/n\n')
+            if choice == 'y':
+                score = self.random_cart(score, False)
+                if bot_score < 19 and score <= 21:
+                    bot_score = self.random_cart(bot_score, True)
+                if score > 21 or bot_score == 21:
+                    print('Извините, вы проиграли')
+                    break
+                elif score == 21 and bot_score == 21:
+                    print('Ничья. Так тоже бывает')
+                elif score == 21 or bot_score > 21:
+                    print('Поздравляем! Вы победили!')
+                    break 
+            elif choice == 'n':
+                if score > bot_score and bot_score < 19:
+                    while bot_score < 19:
+                        bot_score = self.random_cart(bot_score, True)
+                if score < bot_score <= 21:
+                    print(f'Вы поиграли, у вас {score} очков, а у крупье {bot_score} очков')
+                else:
+                    print(f'Вы победили, у вас {score} очков, а у крупье {bot_score} очков') 
+                break
 
+    def start(self):
+        random.shuffle(self.deck)
+        print('Игра BlackJack началась')
+        print('В blackjack десятки, валеты, дамы и короли стоят по 10 очков.\n Туз может стоить 1 или 11 очков')
+        print('----------------------')
+        self.choice()
+        print('До скорого!')
+    
+black_jack = BlackJack()
+black_jack.start()
+          
 
